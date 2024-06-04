c5915b0353489606a38eac33a6bb8e2287fa52ff
kevin <kevinke386@gmail.com>
2024-06-04 10:32:21 +0300

    Updated

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+   def __init__(self,name):
+      self.name= ''
+      self.breed = ''
+   def get_name(self):
+      print('Getting age....')
+      return self.name
+   
+   def set_name(self,name):
+      if(type(name) == ''):
+         print(f'Setting name to {name}')
+         self.name = name
+      else:
+         print('Name must be a string')
+
+   name = property(get_name,set_name,)
+      
+class breed:
+   def __init__(self,name):
+      self.name = name
+      self.breed = ''
+   def get_breed(self):
+      print('Getting breed....')
+      return self.breed
+   def set_breed(self,breed):
+      if (breed in APPROVED_BREEDS):
+         print(f'Setting breed to {breed}')
+      else:
+         print("Breed not approved")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.job = ''
+    
+    def get_job(self):
+        return self.job
+
+   
+    def set_job(self, job):
+        if (job in APPROVED_JOBS):
+            self.job = job.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def get_name(self):
+        return self._name
+
+    
+    def set_name(self, value):
+        if not isinstance(value, str) or len(value) > 25:
+            print("Name must be a string under 25 characters.")
+            return
+        self._name = value.title()
